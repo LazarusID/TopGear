83a7621c9219a0b4cd1ac3930d44a9bb200168d7
Clay Dowling <clay@lazarusid.com>
2017-05-07 17:11:24 -0400

    Squashed commit of the following:
    commit 85a6bfc8a99cfa544d428413b6917b910b8af331
    Author: Clay Dowling <clay@lazarusid.com>
    Date:   Sun May 7 17:10:47 2017 -0400
    
        Updated documentation, ensured that testing works.

---
+++ java/src/test/java/GearBoxTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by cdowling on 5/7/17.
+ */
+public class GearBoxTest {
+
+    @Test
+    public void isTest() {
+        assertThat(true, is(true));
+    }
+
+}
