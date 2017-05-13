c8a83df3d3cb21bdbfb7d1d402082ee4cf3860df
Clay Dowling <clay@lazarusid.com>
2017-05-12 20:29:54 -0400

    Added full Characterization Test suite and target interface

--- java/src/main/java/GearBox.java
+++ java/src/main/java/GearBox.java
@@ -2,25 +2,28 @@
 
 public class GearBox {
 
-	private int s = 0;
-	private int e = 0;
+    private int gear = 0;
 
-	public void doit(int i) {
-		if (s < 0) {
-			// do nothing!
-			e = i;
-		} else {
-			if (s > 0) {
-				if (i > 2000)
-					s++;
-				} else if (i < 500) {
-					s--;
-				}
-			} if (s > 6) {
-				s--;
-			} else if (s < 1) {
-				s++;
-			}
-			e = i;
-		}
+    public void doit(int i) {
+        if (gear > 0) {
+            if (i > 2000)
+                gear++;
+            else if (i < 500) {
+                gear--;
+            }
+        }
+        if (gear > 6) {
+            gear--;
+        } else if (gear < 1) {
+            gear++;
+        }
+    }
+
+    public int getCurrentGear() {
+        return gear;
+    }
+
+    public void setGearRange(int gear, int shiftDown, int shiftUp) {
+        // TODO: Implement Me!
+    }
 }
--- java/src/test/java/GearBoxTest.java
+++ java/src/test/java/GearBoxTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -9,9 +10,108 @@ import static org.junit.Assert.*;
  */
 public class GearBoxTest {
 
+    GearBox box;
+
+    @Before
+    public void setUp() {
+        box = new GearBox();
+    }
+
     @Test
-    public void isTest() {
-        assertThat(true, is(true));
+    public void getCurrentGear_byDefault_returnsNeutral() {
+        assertThat(box.getCurrentGear(), is(0));
+    }
+
+    @Test
+    public void getCurrentGear_afterDoItWithRpmLessThanTwoThousand_returnsFirst() {
+        box.doit(1000);
+        assertThat(box.getCurrentGear(), is(1));
+    }
+
+    @Test
+    public void getCurrentGear_startingFromNeutralAndDoItOverTwoThousand_returnsFirst() {
+        shiftUp();
+        assertThat(box.getCurrentGear(), is(1));
+    }
+
+    @Test
+    public void getCurrentGear_afterShiftUpTwice_returnsSecond() {
+        shiftUp();
+        shiftUp();
+        assertThat(box.getCurrentGear(), is(2));
+    }
+
+    @Test
+    public void getCurrentGear_afterShiftUpThreeTimes_returnsThird() {
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        assertThat(box.getCurrentGear(), is(3));
+    }
+
+    @Test
+    public void getCurrentGear_afterShiftUpFourTimes_returnsFourth() {
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        assertThat(box.getCurrentGear(), is(4));
+    }
+
+    @Test
+    public void getCurrentGear_afterShiftUpFiveTimes_returnsFifth() {
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();;
+        assertThat(box.getCurrentGear(), is(5));
+    }
+
+    @Test
+    public void getCurrentGear_afterShiftUpSixTimes_returnsSixth() {
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        assertThat(box.getCurrentGear(), is(6));
+    }
+
+    @Test
+    public void getCurrentGear_afterShiftUpSevenTimes_returnsSixth() {
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        shiftUp();
+        assertThat(box.getCurrentGear(), is(6));
+    }
+
+    @Test
+    public void getCurrentGear_afterShiftUpTwiceAndShiftDown_returnsFirst() {
+        shiftUp();
+        shiftUp();
+        shiftDown();
+        assertThat(box.getCurrentGear(), is(1));
+    }
+
+    @Test
+    public void getCurrentGear_afterShiftUpAndShiftDown_returnsFirst() {
+        shiftUp();
+        shiftDown();
+        assertThat(box.getCurrentGear(), is(1));
+    }
+
+    private void shiftUp() {
+        box.doit(2001);
+    }
+
+    private void shiftDown() {
+        box.doit(200);
     }
 
 }
